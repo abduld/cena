07b33360130aaae262132b0ba815f9bd00a6a6ca
abdul dakkak <adakkak@gmail.com>
2014-10-29 23:40:53 -0500

    remove dead files

--- ast/node/assign_test.cpp
+++
@@ -1,9 +0,0 @@
-#include "ast.hpp"
-
-TEST(Assign, CanCreate) {
-  shared_ptr<AssignNode> nd(new AssignNode());
-  ASSERT_EQ(nd->isAtom(), false);
-  ASSERT_EQ(nd->isStatement(), false);
-  nd->setLHS(shared_ptr<IdentifierNode>(new IdentifierNode("v")));
-  nd->setRHS(shared_ptr<IntegerNode>(new IntegerNode(2)));
-}
--- ast/node/type_test.cpp
+++
@@ -1,8 +0,0 @@
-#include "ast.hpp"
-
-TEST(Type, CanCreate) {
-  shared_ptr<TypeNode> nd(new TypeNode());
-  nd->addQualifyer("static");
-  nd->addQualifyer("const");
-  nd->addBase("int");
-}
