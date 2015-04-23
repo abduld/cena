e01fa87f0320153804c5c7af7f0af61dc6baf3f4
Abdul Dakkak <abduld@wolfram.com>
2015-04-23 17:02:22 -0500

    special config

--- cmake/modules/FindLLVM.cmake
+++ cmake/modules/FindLLVM.cmake
@@ -10,8 +10,9 @@
 #  LLVM_MODULE_LIBS  - list of llvm libs for working with modules.
 
 find_program(LLVM_CONFIG_EXECUTABLE
-  NAMES llvm-config-3.6
+  NAMES llvm-config-3.6 llvm-config
   PATHS
+  /Users/dddscy/project/sys/bin
   /usr/local/opt/llvm36/bin
   /Users/abduld/usr/bin
 )
